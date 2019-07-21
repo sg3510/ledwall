c3ba632b97913b1143b9c72f66b8a3af455149a4
Seb <sebgrubb@gmail.com>
2019-07-20 19:39:37 -0700

    ADC Test

---
+++ ADC_Test.py
@@ -0,0 +1,14 @@
+#Setup
+import board
+import busio
+i2c = busio.I2C(board.SCL, board.SDA)
+import adafruit_ads1x15.ads1115 as ADS
+from adafruit_ads1x15.analog_in import AnalogIn
+
+ads = ADS.ADS1115(i2c)
+
+chan0 = AnalogIn(ads, ADS.P0)
+chan1 = AnalogIn(ads, ADS.P1)
+chan2 = AnalogIn(ads, ADS.P2)
+chan3 = AnalogIn(ads, ADS.P3)
+print(chan.value, chan.voltage)
---
+++ untitled.py
@@ -0,0 +1,16 @@
+#Setup
+import board
+import busio
+i2c = busio.I2C(board.SCL, board.SDA)
+import adafruit_ads1x15.ads1115 as ADS
+from adafruit_ads1x15.analog_in import AnalogIn
+
+ads = ADS.ADS1115(i2c)
+
+chan0 = AnalogIn(ads, ADS.P0)
+chan1 = AnalogIn(ads, ADS.P1)
+chan2 = AnalogIn(ads, ADS.P2)
+chan3 = AnalogIn(ads, ADS.P3)
+
+while True:
+	print(chan0.voltage, chan1.voltage, chan2.voltage, chan3.voltage)
