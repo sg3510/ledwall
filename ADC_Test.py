a2513cb1415de438cf9ae1f6e83fc918fca928e6
Seb <sebgrubb@gmail.com>
2019-07-20 20:15:28 -0700

    format fix

--- ADC_Test.py
+++ ADC_Test.py
@@ -7,6 +7,8 @@ import adafruit_ads1x15.ads1115 as ADS
 from adafruit_ads1x15.analog_in import AnalogIn
 
 ads = ADS.ADS1115(i2c)
+ads.overRideConversionDelay(65);
+
 
 chan0 = AnalogIn(ads, ADS.P0)
 chan1 = AnalogIn(ads, ADS.P1)
@@ -15,11 +17,7 @@ chan3 = AnalogIn(ads, ADS.P3)
 
 while True:
 	a = chan0.voltage
-	sleep(0.1)
 	b = chan1.voltage
-	sleep(0.1)
 	c = chan2.voltage
-	sleep(0.1)
 	d = chan3.voltage
-	sleep(0.1)
-	print("{0:2.2} - {0:2.2} - {0:2.2} - {0:2.2}".format(a,b,c,d))
+	print("{0:2.2f} - {1:2.2f} - {2:2.2f} - {3:2.2f}".format(a,b,c,d))
